75f7e48f3b8d9935444a208995145930ed8e0e48
larabee90 <lauradouglas90@gmail.com>
2017-12-21 23:18:34 -0330

    Changed range sliders, styled jQueryUI elements

--- authorization_code/public/js/main.js
+++ authorization_code/public/js/main.js
@@ -98,6 +98,75 @@ $( document ).ready(function () {
             })
 
         })
-    }, false)
+    }, false);
+
+    // range sliders
+    $( "#dancability" ).slider({
+        range: true,
+        min: 0.0,
+        max: 1.0,
+        step: 0.1,
+        values: [ 0, 1.0 ],
+        slide: function( event, ui ) {
+            $( "#amt1" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+    });
+    $( "#amt1" ).val(  $( "#dancability" ).slider( "values", 0 ) +
+        " - " + $( "#dancability" ).slider( "values", 1 ) );
+
+    $( "#energy" ).slider({
+        range: true,
+        min: 0.0,
+        max: 1.0,
+        step: 0.1,
+        values: [ 0, 1.0 ],
+        slide: function( event, ui ) {
+            $( "#amt2" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+    });
+    $( "#amt2" ).val(  $( "#energy" ).slider( "values", 0 ) +
+        " - " + $( "#energy" ).slider( "values", 1 ) );
+
+    $( "#instrumental" ).slider({
+        range: true,
+        min: 0.0,
+        max: 1.0,
+        step: 0.1,
+        values: [ 0, 1.0 ],
+        slide: function( event, ui ) {
+            $( "#amt3" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+    });
+    $( "#amt3" ).val(  $( "#instrumental" ).slider( "values", 0 ) +
+        " - " + $( "#instrumental" ).slider( "values", 1 ) );
+
+    $( "#tempo" ).slider({
+        range: true,
+        min: 80,
+        max: 203,
+        step: 1,
+        values: [ 80, 203 ],
+        slide: function( event, ui ) {
+            $( "#amt4" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+    });
+    $( "#amt4" ).val(  $( "#tempo" ).slider( "values", 0 ) +
+        " - " + $( "#tempo" ).slider( "values", 1 ) );
+
+    $( "#valance" ).slider({
+        range: true,
+        min: 0,
+        max: 1.0,
+        step: 0.1,
+        values: [ 0, 1.0 ],
+        slide: function( event, ui ) {
+            $( "#amt5" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+    });
+    $( "#amt5" ).val(  $( "#valance" ).slider( "values", 0 ) +
+        " - " + $( "#valance" ).slider( "values", 1 ) );
+
+
+
 
 });
